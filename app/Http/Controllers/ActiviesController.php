7354d091571d4c3a3865293267452108c515e66c
user-lkun <1026860145@qq.com>
2018-08-05 21:41:51 +0800

    8.05静态页面

--- app/Http/Controllers/ActiviesController.php
+++ app/Http/Controllers/ActiviesController.php
@@ -11,10 +11,18 @@ class ActiviesController extends Controller
 
 //        $activies = Activies::all();
         //商户端只显示没有过期的活动
+
         $activies = Activies::where('end_time','>',time())->paginate();
-        return view('activies/index',compact('activies'));
+//        return view('activies/index',compact('activies'));
+        $contents = view('activies/index',compact('activies'));
+        file_put_contents('index.html',$contents);
+        return redirect('index.html');
+
     }
     public function show(Activies $activy){
-        return view('activies/show',compact('activy'));
+//        return view('activies/show',compact('activy'));
+        $contents = view('activies/show',compact('activy'));
+        file_put_contents('show.html',$contents);
+        return redirect('show.html');
     }
 }
